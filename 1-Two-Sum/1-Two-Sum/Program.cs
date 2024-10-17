f1973283a2b2ba08d9699558a53db8c192669baf
Siarhei Biadniuk <sergeybednyuk@gmail.com>
2024-10-17 19:38:41 +0200

    add projects

---
+++ 1-Two-Sum/1-Two-Sum/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _1_Two_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums1 = new int[] { 3, 2, 4 };
+            int target1 = 6;
+
+            var r1 = TwoSum(nums1, target1);
+            foreach (int x in r1)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadLine();
+
+            int[] TwoSum(int[] nums, int target)
+            {
+                var result = new int[2] { -1, -1};
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (result[0] != -1)
+                    {
+                        break;
+                    }
+                    for (int j = 0; j < nums.Length; j++)
+                    {
+                        if (nums[i] + nums[j] == target && i != j)
+                        {
+                            result[0] = i;
+                            result[1] = j;
+                            break;
+                        }
+                    }
+                }
+
+                return result;
+            }
+        }
+    }
+}
---
+++ 217-Contain-Duplicate/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _217_Contain_Duplicate;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var case1 = new int[] { 1, 2, 3, 1 };
+        var case2 = new int[] { 1, 2, 3, 4 };
+        var case3 = new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+
+        Console.WriteLine(ContainsDuplicateBySort(case1));
+        Console.WriteLine(ContainsDuplicateBySort(case2));
+        Console.WriteLine(ContainsDuplicateBySort(case3));
+        
+        System.Console.ReadKey();
+    }
+
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var item = nums[i];
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (i != j && item == nums[j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //My best solution
+    public static bool ContainsDuplicateBySort(int[] nums)
+    {
+        Array.Sort(nums);
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] == nums[i -1])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
