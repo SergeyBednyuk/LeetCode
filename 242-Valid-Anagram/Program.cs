a12137f585e90bffce0973173800e5ff660fc94e
Siarhei Biadniuk <sergeybednyuk@gmail.com>
2024-10-24 16:07:42 +0200

    Add 242 Valid Anagram solution

---
+++ 242-Valid-Anagram/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _242_Valid_Anagram;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var s = "aacc";
+		var t = "ccac";
+
+		bool IsAnagram(string s, string t)
+		{
+			if(s.Length != t.Length)
+			{
+				return false;
+			}
+
+			var arrS = s.ToArray();
+			var arrT = t.ToArray();
+			Array.Sort(arrS);
+			Array.Sort(arrT);
+			
+			return arrS.SequenceEqual(arrT);
+		}
+
+
+	}
+}
